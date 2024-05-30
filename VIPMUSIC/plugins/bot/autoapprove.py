17238fb4b899fe69bea9ac5e96be0dab554ba094
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-30 17:28:51 +0530

    Update autoapprove.py

--- VIPMUSIC/plugins/bot/autoapprove.py
+++ VIPMUSIC/plugins/bot/autoapprove.py
@@ -13,11 +13,11 @@ from pyrogram.enums import ParseMode
 from typing import Union, Optional
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/95281ac786e89c37d3edd.jpg",
+    "https://telegra.ph/file/9aeee620ac8be868e0fe6.jpg",
+    "https://telegra.ph/file/de27925c1fea494ec1655.jpg",
+    "https://telegra.ph/file/f86bac9f1576697c69669.jpg",
+    "https://telegra.ph/file/ea3c012fdde82812a5a73.jpg",
 ]
 
 # --------------------------------------------------------------------------------- #
