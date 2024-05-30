f9cfd216529a021f40c58e894c1cb99a539a38ce
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-30 17:20:18 +0530

    Update left.py

--- VIPMUSIC/cplugin/left.py
+++ VIPMUSIC/cplugin/left.py
@@ -31,11 +31,11 @@ import random
 import asyncio
 
 random_photo = [
-    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
-    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
-    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
-    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
-    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+    "https://telegra.ph/file/a13a059b486d096cbc7ba.jpg",
+    "https://telegra.ph/file/e2a0c222e19c1cab36271.jpg",
+    "https://telegra.ph/file/f86bac9f1576697c69669.jpg",
+    "https://telegra.ph/file/ea3c012fdde82812a5a73.jpg",
+    "https://telegra.ph/file/57d5705756c589e14a1a3.jpg",
 ]
 
 bg_path = "VIPMUSIC/assets/userinfo.png"
