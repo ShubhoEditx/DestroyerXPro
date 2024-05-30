ab6ebe219462e9de28abfd04df1d5669120d5d00
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-30 13:04:19 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -8,19 +8,19 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+    await msg.reply("**😍𝐕ɪᴅᴇᴏ 𝐂ʜᴀᴛ 𝐒ᴛᴀʀᴛᴇᴅ🥳**")
 
 
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+    await msg.reply("**😕𝐕ɪᴅᴇᴏ 𝐂ʜᴀᴛ 𝐄ɴᴅᴇᴅ💔**")
 
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app: app, message: Message):
-    text = f"➻ {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :**\n\n**➻ **"
+    text = f"➻ {message.from_user.mention}\n\n**๏ 𝐈ɴᴠɪᴛɪɴɢ 𝐈ɴ 𝐕ᴄ 𝐓ᴏ :**\n\n**➻ **"
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
